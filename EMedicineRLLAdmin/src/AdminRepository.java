dfdc7b37fffbe7b2f1e3afe019817f0a47d128f6
omsesankhanumolu <97578487+omsesankhanumolu@users.noreply.github.com>
2022-10-29 17:18:17 +0530

    Update AdminRepository.java

--- EMedicineRLLAdmin/src/AdminRepository.java
+++ EMedicineRLLAdmin/src/AdminRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Admin;
-//import com.example.demo.domain.User;
 
 
 
